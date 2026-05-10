114b2062d44fde0cd951133e3fca4dc03d278ea1
user <q@q>
2026-05-10 11:57:45 +0500

    100% vsego plszssssssss

--- visualiser/visualiser.cpp
+++ visualiser/visualiser.cpp
@@ -96,7 +96,7 @@ void Visualiser::draw(std::vector<GameMap> &screens) {
         drawExit(map.exitX, map.exitY);
         drawRobot(map.rX,map.rY);
         window.display();
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     }
 
 }
